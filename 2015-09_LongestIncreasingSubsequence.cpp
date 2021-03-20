975562dc0f1f039be0da479b3a7a3d38f5e62bfe
Antonina Shih <zehir.anton@gmail.com>
2021-03-21 04:30:24 +0800

    Add files

---
+++ 2015-09_LongestIncreasingSubsequence.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+// for (i = 1 to n) begin
+//   LIS[i] = 1;
+//   for (j = i-1 to 1) begin
+//     if (LIS[j]+1 > LIS[i] && ...) begin
+//       ...
+//     end
+//   end
+// end
+
+int main() {
+    int n;
+    while (cin >> n) {
+        int array[n];
+        int LIS[n];
+        for(int i=0; i<n; i++) {
+            cin >> array[i];
+            LIS[i] = 1;
+        }
+        int count = 1;
+        for(int i=1; i<n; i++) {
+            for(int j=i-1; j>=0; j--) {
+                if (array[i] > array[j]) {
+                    if(LIS[i] == LIS[j]) {
+                        LIS[i]++;
+                    } else if (LIS[i] < LIS[j]) {
+                        LIS[i] += LIS[j];
+                    }
+                    if (LIS[i] > count){
+                        count = LIS[i];
+                    }
+                }
+            }
+        }
+        // for(int i=0; i<n; i++) {
+        //     cout << LIS[i];
+        // }
+        cout << count << endl;
+    }
+    return 0;
+}
---
+++ 22181_Dollars.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+// 100.00 50.00 20.00 10.00 5.00 2.00 1.00 0.50 0.20 0.10 0.05
+
+int main() {
+    float dollar;
+    while (cin >> dollar) {
+        /* code */
+    }
+    
+}

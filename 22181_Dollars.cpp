c15a2b573e739b96dc2e9264509fb02c5214a426
Julian Shih <zehir.anton@gmail.com>
2021-08-16 12:30:33 +0800

    Update

--- 22181_Dollars.cpp
+++ 22181_Dollars.cpp
@@ -1,21 +1,21 @@
 #include <iostream>
+#include <cstdio>
 using namespace std;
 
 // 100.00 50.00 20.00 10.00 5.00 2.00 1.00 0.50 0.20 0.10 0.05
 int coins[11] = {10000, 5000, 2000, 1000, 500, 200, 100, 50, 20, 10, 5};
 
 int main() {
-    double dollar;
-    while (cin >> dollar && dollar != 0.00) {
-        dollar *= 100;
-        int dp[30006] = {1};
-        dp[0] = 1;
-        for(int i = 0; i<11; i++) {
-            for(int j = coins[i]; j<=30000; j++) {
-                dp[j] += dp[j - coins[i]];
-            }
+ int dollar = 0, cent = 0;
+   while(scanf("%d.%d", &dollar, &cent) != EOF && !(dollar == 0 && cent == 0)) {
+    long long int dp[30005] = {1};
+    dp[0] = 1;
+    for(int i = 0; i<11; i++) {
+        for(int j = coins[i]; j<=30000; j++) {
+            dp[j] += dp[j - coins[i]];
         }
-        cout << dp[(int)dollar] << endl;
     }
-    return 0;
+    printf("%3d.%02d%17lld\n", dollar, cent, dp[dollar*100 + cent]);
+   }
+  return 0;
 }
